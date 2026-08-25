f1acbd0b77cca600a976b3711173952fb5225fa4
athasirajudin <137172350+athasirajudin@users.noreply.github.com>
2026-08-25 16:55:21 +0700

    style(auth): clean login request imports

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests\Auth;
 
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
