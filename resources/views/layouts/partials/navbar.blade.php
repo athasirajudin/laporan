90a0fe5b5d4bc876c79de3a0e97ca3d2a6d79e8f
athasirajudin <137172350+athasirajudin@users.noreply.github.com>
2026-08-25 16:54:45 +0700

    feat(auth): add login and logout actions to navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -2,10 +2,19 @@
     <div class="container-fluid">
         <a class="navbar-brand fw-semibold" href="{{ route('home') }}">Sistem Pendataan Kos</a>
 
-        @auth
-            <span class="navbar-text text-white">
-                {{ auth()->user()->name }}
-            </span>
-        @endauth
+        <div class="ms-auto d-flex align-items-center gap-3">
+            @auth
+                <span class="navbar-text text-white">
+                    {{ auth()->user()->name }}
+                </span>
+
+                <form method="POST" action="{{ route('logout') }}" class="m-0">
+                    @csrf
+                    <button type="submit" class="btn btn-outline-light btn-sm">Logout</button>
+                </form>
+            @else
+                <a href="{{ route('login') }}" class="btn btn-light btn-sm">Login</a>
+            @endauth
+        </div>
     </div>
 </nav>
