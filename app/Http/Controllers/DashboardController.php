c94e29cdf56db3263426f3f7afbf81253d301065
athasirajudin <137172350+athasirajudin@users.noreply.github.com>
2026-08-28 17:03:37 +0700

    fix: handle invalid dashboard role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke(): View|RedirectResponse
+    public function __invoke(): View|Response
     {
         return match (auth()->user()->role) {
             'super_admin' => view('dashboard.super-admin'),
             'admin' => view('dashboard.admin'),
             'pemilik_kos' => view('dashboard.pemilik-kos'),
-            default => auth()->logout() ?: redirect()->route('login'),
+            default => abort(403),
         };
     }
 }
