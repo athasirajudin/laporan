58f9e120b912c747bab778594cb2ddb22bdefa00
athasirajudin <137172350+athasirajudin@users.noreply.github.com>
2026-08-28 17:11:36 +0700

    feat(super-admin): add super admin routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SuperAdmin\AdminController;
+use App\Http\Controllers\SuperAdmin\WilayahController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
@@ -14,4 +16,25 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+    Route::middleware('role:super_admin')->prefix('super-admin')->name('super-admin.')->group(function () {
+        Route::prefix('admin')->name('admin.')->group(function () {
+            Route::get('/', [AdminController::class, 'index'])->name('index');
+            Route::get('/create', [AdminController::class, 'create'])->name('create');
+            Route::post('/', [AdminController::class, 'store'])->name('store');
+            Route::get('/{admin}', [AdminController::class, 'show'])->name('show');
+            Route::get('/{admin}/edit', [AdminController::class, 'edit'])->name('edit');
+            Route::put('/{admin}', [AdminController::class, 'update'])->name('update');
+            Route::patch('/{admin}/status', [AdminController::class, 'toggleStatus'])->name('status');
+        });
+
+        Route::prefix('wilayah')->name('wilayah.')->group(function () {
+            Route::get('/', [WilayahController::class, 'index'])->name('index');
+            Route::get('/create', [WilayahController::class, 'create'])->name('create');
+            Route::post('/', [WilayahController::class, 'store'])->name('store');
+            Route::get('/{wilayah}', [WilayahController::class, 'show'])->name('show');
+            Route::get('/{wilayah}/edit', [WilayahController::class, 'edit'])->name('edit');
+            Route::put('/{wilayah}', [WilayahController::class, 'update'])->name('update');
+        });
+    });
 });
